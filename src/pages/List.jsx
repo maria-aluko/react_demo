24621d2c62a26635754ba998d6e7ca4ef32aa2db
Maria Aluko <maria.alukoo@gmail.com>
2024-11-27 12:29:48 +0200

    add useEffect and fetching data from my own server

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import personsData from "../data/personsData.js";
+import { useEffect, useState } from "react";
 import Card from "../components/Card.jsx";
 
 const List = () => {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() = {}, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/persons")
+    .then((response) => response.json())
+    .then((data) => {
+      setPosts(data);
+      setIsLoading(false);
+    })
+  }, []);
 
   const handleNavigate = (id) => {
     navigate(`/${id}`);
@@ -15,16 +28,20 @@ const List = () => {
           <>
             <h2>Employees</h2>
             <div className="list">
-              {personsData.map((person) => (
-                <Card
-                  key={person.id}
-                  firstName={person.firstName}
-                  title={person.title}
-                  age={person.age}
-                  animal={person.animal}
-                  onClick={() => handleNavigate(person.id)}
-                />
-              ))}
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                posts.map((person) => (
+                  <Card
+                    key={person.id}
+                    firstName={person.firstName}
+                    title={person.title}
+                    age={person.age}
+                    animal={person.animal}
+                    onClick={() => handleNavigate(person.id)}
+                  />
+                ))
+              )}
             </div>
           </>
         </main>
